0e0ed9af6318a2dcf3923a10da0cb5898db8cfc9
Niteesh01 <niteesh.iit@gmail.com>
2017-09-06 19:53:05 -0400

    "Main and GamePanel"

--- Breakout(Command Pattern)/src/edu/iu/P532/recieverclass/Main.java
+++ Breakout(Command Pattern)/src/edu/iu/P532/recieverclass/Main.java
@@ -1,5 +1,34 @@
 package edu.iu.P532.recieverclass;
 
+import javax.swing.JFrame;
+
+
+
 public class Main {
 
+	public static final int WIDTH = 800, HEIGHT = 800;
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Breakout");
+		
+		
+		GamePanel panel = new GamePanel();
+		
+		
+		//For Centering the window on the screen
+		frame.setLocationRelativeTo(null);
+		
+		//setting the size of the frame
+		frame.setSize(WIDTH, HEIGHT);
+		
+		//adding the Jpanel to the Frame
+		frame.add(panel);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setVisible(true);
+		
+		
+	}
+	
 }
