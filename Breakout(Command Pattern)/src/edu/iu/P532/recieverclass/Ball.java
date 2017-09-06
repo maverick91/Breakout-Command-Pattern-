18099f329481bbf252c9268bbb818b43a5129e55
Niteesh01 <niteesh.iit@gmail.com>
2017-09-06 18:39:11 -0400

    testing commit(ball class)

--- Breakout(Command Pattern)/src/edu/iu/P532/recieverclass/Ball.java
+++ Breakout(Command Pattern)/src/edu/iu/P532/recieverclass/Ball.java
@@ -1,5 +1,48 @@
 package edu.iu.P532.recieverclass;
 
-public class Ball {
+import java.awt.Color;
+import java.awt.Graphics;
 
+import edu.iu.P532.recieverclass.GameConstants;
+
+public class Ball {
+	protected int ballposX ;
+	protected int ballposY ;
+	private int ballRadius;
+	protected int ballXdir = GameConstants.BALL_SPEED_XDIR;
+	protected int ballYdir = GameConstants.BALL_SPEED_XDIR;
+
+	
+	//Constructor for the Ball
+	public Ball(int ballInitialPositionX, int ballInitialPositionY, int ballRadius) {
+		this.ballposX = ballInitialPositionX;
+		this.ballposY = ballInitialPositionY;
+		this.ballRadius = ballRadius;
+	}
+	
+	
+	public int getBallposX() {
+		return ballposX;
+	}
+
+
+	public void setBallposX(int ballposX) {
+		this.ballposX = ballposX;
+	}
+	
+	public int getBallposY() {
+		return ballposY;
+	}
+
+
+	public void setBallposY(int ballposY) {
+		this.ballposY = ballposY;
+	}
+
+
+	//Draws the Ball
+	public void draw(Graphics g){
+		g.setColor(Color.RED);
+        g.fillOval(ballposX, ballposY, ballRadius, ballRadius);
+	}
 }
