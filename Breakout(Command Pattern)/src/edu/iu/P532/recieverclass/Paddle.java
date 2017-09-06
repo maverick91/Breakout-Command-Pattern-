b2414790a6b7d9a4ddeb192048946fdcb1b3629a
Niteesh01 <niteesh.iit@gmail.com>
2017-09-06 19:52:31 -0400

    "main and GamePanel classes"

--- Breakout(Command Pattern)/src/edu/iu/P532/recieverclass/Paddle.java
+++ Breakout(Command Pattern)/src/edu/iu/P532/recieverclass/Paddle.java
@@ -1,18 +1,30 @@
 package edu.iu.P532.recieverclass;
 
-public class Paddle {
-	private int locX=0;
-	
-	private int speed = 5;
-	
-	public void move() 
-	{
-		locX=locX+speed;
-	}
+import java.awt.Color;
+import java.awt.Graphics;
+import edu.iu.P532.recieverclass.GameConstants;
 
-	public void unMove() 
-	{
-		locX=locX-speed;
-	}
-	
+
+public class Paddle  {
+//private int xSpeed;
+
+protected int paddleXPos;
+protected int paddleYPos;
+private int paddleWidth;
+private int paddleHeight;
+
+   // Constructor
+   public Paddle(int paddleInitialPositionX, int paddleInitialPositionY, int paddleWidth, int paddleHeight) {
+   	    this.paddleXPos = paddleInitialPositionX;
+		this.paddleYPos = paddleInitialPositionY;
+		this.paddleWidth = paddleWidth;
+		this.paddleHeight = paddleHeight;			
+   }
+
+   // Draws the paddle
+   public void draw(Graphics g) {
+       g.setColor(Color.GREEN);
+       g.fillRect(paddleXPos,paddleYPos,paddleWidth,paddleHeight);
+   }
+   
 }
