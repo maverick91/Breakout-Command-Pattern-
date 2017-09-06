0f3828f0ed7860af8e7b7f9c579cce17f76f3505
Niteesh01 <niteesh.iit@gmail.com>
2017-09-06 19:05:19 -0400

    test2

--- Breakout(Command Pattern)/src/edu/iu/P532/recieverclass/GameConstants.java
+++ Breakout(Command Pattern)/src/edu/iu/P532/recieverclass/GameConstants.java
@@ -1,5 +1,29 @@
 package edu.iu.P532.recieverclass;
 
 public class GameConstants {
+	// Size of the window
+    public static final int WINDOW_WIDTH = 800;
+    public static final int WINDOW_HEIGHT = 800;
+ 
+    // Ball
+    public static final int BALL_RADIUS = 20;
+    public static final int BALL_INITIAL_POSITION_X = 395;
+    public static final int BALL_INITIAL_POSITION_Y = 410;
+    
+    // ball velocity
+    public static final int BALL_SPEED_XDIR = -2;
+    public static final int BALL_SPEED_YDIR = -2;
+
+    // Paddle
+    public static final int PADDLE_WIDTH = 110;
+    public static final int PADDLE_HEIGHT = 10;
+    public static final int PADDLE_INITIAL_POSITION_X = 360;
+    public static final int PADDLE_INITIAL_POSITION_Y = 680;
+  
+    // Bricks
+    public static final int BRICK_WIDTH = 70;
+    public static final int BRICK_HEIGHT = 40;
+    public static final int NUM_ROWS = 5;
+    public static final int NUM_COLUMNS = 10;
 
 }
